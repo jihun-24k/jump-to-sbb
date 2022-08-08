b3d366286f8c360fd9e63c8625232a890e169a99
jihun-24k <gi020477@gmail.com>
2022-08-08 13:37:43 +0900

    Feat: 간단한 요청 문제 풀어보기

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,15 +1,34 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+    private int rs = -1;
 
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public String increase() {
+        return """
+                 <h1> %d</h1>
+                 """.formatted(++rs);
     }
 }
