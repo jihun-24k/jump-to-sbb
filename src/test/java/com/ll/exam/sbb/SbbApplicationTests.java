cc5f3f899b77694a603e3704fe63b18d562bc999
jihun-24k <gi020477@gmail.com>
2022-08-10 13:47:23 +0900

    Test: 2-05 findBySubject 테스트 코드 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -53,4 +53,9 @@ class SbbApplicationTests {
 			assertEquals("sbb가 무엇인가요?", q.getSubject());
 		}
 	}
+	@Test
+	void testFindBySubject() {
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
 }
