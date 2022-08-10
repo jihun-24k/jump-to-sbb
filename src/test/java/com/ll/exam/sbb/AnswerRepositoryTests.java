a90d0dc81de072b6674c04c013097fb237a1dd26
jihun-24k <gi020477@gmail.com>
2022-08-10 15:32:17 +0900

    Test: 2-05 Answer Test, Transactional 어노테이션 추가

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -3,8 +3,10 @@ package com.ll.exam.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,5 +38,19 @@ public class AnswerRepositoryTests {
         assertTrue(oa.isPresent());
         Answer a = oa.get();
         assertEquals(2, a.getQuestion().getId());
+    }
+
+    @Test
+    @Transactional
+    void testFindByIdWhere() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        // null 테스트
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
