bd05435c2230896fecf0b8a9903097b2a4bb4270
jihun-24k <gi020477@gmail.com>
2022-08-10 13:45:07 +0900

    Test: 2-05 findById 테스트 데이터 코드 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,15 @@ class SbbApplicationTests {
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testFindById() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		// NULL이 아닌지를 확인 isPresent()
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 }
