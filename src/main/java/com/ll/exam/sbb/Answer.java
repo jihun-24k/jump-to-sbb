3cf2b374e3363bcf066ae26d13d3ea3382c99ca3
jihun-24k <gi020477@gmail.com>
2022-08-10 11:35:29 +0900

    Feat: Answer에서 Question의 DB 관계 설정

--- src/main/java/com/ll/exam/sbb/Answer.java
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -1,7 +1,6 @@
 package com.ll.exam.sbb;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.persistence.*;
 
@@ -11,19 +10,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question question; // 질문의 주소값
 }
