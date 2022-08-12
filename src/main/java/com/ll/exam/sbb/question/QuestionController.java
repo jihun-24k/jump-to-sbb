7b2d8d57c184d2a59014fb0912c013dd867a590e
jihun-24k <gi020477@gmail.com>
2022-08-12 11:26:36 +0900

    Feat: 리스트 페이지랑 Controller의 값 전달

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,21 +1,24 @@
 package com.ll.exam.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController
 {
-    @Autowired
-    private QuestionService questionService;
+    private final QuestionService questionService;
 
-    @RequestMapping("list")
-    @ResponseBody
-    public String showList() {
-        questionService.findById(1);
-
-        return "HI";
+    @RequestMapping("/question/list")
+    public String showList(Model model) {
+        List<Question> questionList = questionService.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
     }
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -2,13 +2,16 @@ package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.question.Question;
 import com.ll.exam.sbb.question.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
     public Question findById(int id) {
         Question q1 = questionRepository.findById(2).get();
@@ -16,5 +19,8 @@ public class QuestionService {
         System.out.println(q2.getAnswerList());
 
         return q2;
+    }
+    public List<Question> findAll(){
+        return questionRepository.findAll();
     }
 }
