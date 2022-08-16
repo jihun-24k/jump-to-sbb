f9a8a730ebdfdf96d6c0b604e78e516a1777fae2
jihun-24k <gi020477@gmail.com>
2022-08-16 11:48:46 +0900

    Feat: 답변 폼 추가

---
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.ll.exam.sbb.answer;
+
+import com.ll.exam.sbb.question.Question;
+import com.ll.exam.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id);
+        // Todo: 답변을 저장한다.
+        return String.format("redirect:/question/detail/%s".formatted(id));
+    }
+}
