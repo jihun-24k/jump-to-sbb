99a80c2f37bb9ae7f453fbd9c690f84129a280de
jihun-24k <gi020477@gmail.com>
2022-08-24 15:32:42 +0900

    Feat: 내가 구현해본 검색 쿼리문 입력

--- src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,6 +20,20 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
     Page<Question> findBySubjectContains(String kw, Pageable pageable);
 
     Page<Question> findBySubjectContainsOrContentContains(String kw,String kw_, Pageable pageable);
+
+    @Query("select "
+            + "distinct q "
+            + "from Question q "
+            + "left outer join SiteUser u1 on q.author=u1 "
+            + "left outer join Answer a on a.question=q "
+            + "left outer join SiteUser u2 on a.author=u2 "
+            + "where "
+            + "   q.subject like %:kw% "
+            + "   or q.content like %:kw% "
+            + "   or u1.username like %:kw% "
+            + "   or a.content like %:kw% "
+            + "   or u2.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 
     @Transactional
     @Modifying
